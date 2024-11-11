17bb0516ce51be1037c6ce1b0aa53ffb959bbe9c
johnholloway77 <john.holloway@edu.sait.ca>
2024-11-10 18:32:17 -0700

    Added getCard() test

--- test/DeckTest.cpp
+++ test/DeckTest.cpp
@@ -5,6 +5,9 @@
 
 #include <gtest/gtest.h>
 
+#include <iostream>
+
+#include "../include/Card.h"
 #include "../include/Deck.h"
 
 TEST(DeckTest, ConstructorTest) {
@@ -24,5 +27,28 @@ TEST(DeckTest, CreateDeckTest) {
 
   EXPECT_EQ(d->size(), 52);
 
+  delete d;
+}
+
+TEST(DeckTest, GetCardTest) {
+  Deck* d = new Deck();
+
+  d->create();
+  EXPECT_EQ(d->size(), 52);
+
+  Card* c = d->getCard();
+
+  EXPECT_EQ(d->size(), 51);
+
+  EXPECT_TRUE(c);
+
+  EXPECT_TRUE((c->suit >= 0) && (c->suit <= 3));
+  EXPECT_TRUE((c->rank >= 0) && (c->rank <= 13));
+
+  std::cout << "Suit: " + Card::getSuit(c->suit) +
+                   " Rank: " + Card::getRank(c->rank)
+            << std::endl;
+
+  delete c;
   delete d;
 }
