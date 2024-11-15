d376a0433b47eb197a810b4577ef2600b46f6ff7
johnholloway77 <john.holloway@edu.sait.ca>
2024-11-14 23:31:16 -0700

    Removed implementation of virtual function

--- src/Game.cpp
+++ src/Game.cpp
@@ -14,27 +14,6 @@ Game::~Game() {
   // Any necessary cleanup, if required
 }
 
-void Game::dealCards(std::vector<Player*> p) {
-  std::random_device random_device;
-  std::mt19937_64 gen(random_device());
-  std::uniform_int_distribution<> distribution(0, 3);
-
-  int tossed_queen = distribution(gen);
-
-  while (deck->size() > 0) {
-    std::for_each(players.begin(), players.end(),
-                  [this, &tossed_queen](Player* p) {
-                    Card* c = deck->getCard();
-
-                    if (c->rank == Card::QUEEN && c->suit == tossed_queen) {
-                      delete c;
-                      return;
-                    }
-                    p->addCard(c);
-                  });
-  }
-}
-
 void Game::addPlayer(Player* player) {
   players.push_back(player);
 }
