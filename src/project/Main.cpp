58e25eb6b0826395a790ca7ce86b139575b39819
johnholloway77 <john.holloway@edu.sait.ca>
2024-12-07 17:15:56 -0700

    Added main

--- src/project/Main.cpp
+++ src/project/Main.cpp
@@ -4,9 +4,68 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, char **argv) {
-  std::cout << "Hello and goodbye!" << std::endl;
+#include "OldMaid.h"
+#include "OldMaidUI.h"
+#include "Player.h"
+
+#ifdef __linux__
+#include <algorithm>
+#include <limits>
+#endif
+
+int main(int argc, char** argv) {
+  std::cout << "Please enter the number of players (between 2 and 8): ";
+
+  std::vector<Player*> players;
+  int numberOfPlayers;
+
+  while (true) {
+    if (std::cin >> numberOfPlayers) {
+      if (numberOfPlayers >= 2 && numberOfPlayers <= 8) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        break;
+      } else {
+        std::cout << "Out of range. Please enter an integer between 2 and 8: ";
+      }
+    } else {
+      std::cout << "Not a valid integer. Please enter an int between 2 and 8: ";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+
+  for (int i = 0; i < numberOfPlayers; i++) {
+    std::string playerName;
+    std::cout << "Please enter the name of Player " << i + 1 << ": ";
+
+    while (true) {
+      std::getline(std::cin, playerName);
+
+      if (!playerName.empty() &&
+          !std::all_of(playerName.begin(), playerName.end(), isspace)) {
+        break;
+      } else {
+        std::cout << "Invalid player name. Please enter a valid name: ";
+      }
+    }
+
+    players.push_back(new Player(playerName));
+  }
+
+  auto old_maid_ui(new OldMaidUI);
+  auto deck(new Deck);
+
+  auto old_maid_game(new OldMaid(old_maid_ui, deck));
+
+  std::for_each(players.begin(), players.end(),
+                [&old_maid_game](Player* p) { old_maid_game->addPlayer(p); });
+
+  std::cout << "Beginning Old Maid game!" << std::endl;
+
+  old_maid_game->start();
 
   return 0;
 }
